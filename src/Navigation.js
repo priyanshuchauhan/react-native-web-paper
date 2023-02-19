f6e102216b9298643c4ab894ed462fab34460f06
priyanshu <priyanshu911@gmail.com>
2023-02-19 22:18:32 +0530

    config added

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import 'react-native-gesture-handler';
 
+import * as React from "react";
+import { useWindowDimensions, StyleSheet, Text, View } from "react-native";
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Button } from "react-native-paper";
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function MyDrawer() {
+  const dimensions = useWindowDimensions();
+
+  const isLargeScreen = dimensions.width >= 768;
+
+  return (
+    <Drawer.Navigator initialRouteName="Feed">
+      <Drawer.Screen
+        name="HomeStack"
+        component={HomeScreenStack}
+        options={{ drawerLabel: 'Home' }}
+      />
+      <Drawer.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{ drawerLabel: 'Details' }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+function HomeScreenStack({}) {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
       <Button
-        icon="camera"
         mode="contained"
         onPress={() => navigation.navigate("Details")}
       >
@@ -39,15 +74,10 @@ function DetailsScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
-
 function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <MyDrawer/>
     </NavigationContainer>
   );
 }
